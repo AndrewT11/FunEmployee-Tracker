867ee546d9994f910809b2fd96c43a093cc981c0
Andrew Tran <tranandrew@gmail.com>
2021-10-31 21:04:45 -0500

    Working with BCS Assist Push

--- db/query.sql
+++ db/query.sql
@@ -1,40 +1,47 @@
 -- WHEN I choose to VIEW ALL EMPLOYEES
 -- THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
--- SELECT --
---  e.id, 
---  e.first_name, 
---  e.last_name, 
---  r.title, 
---  d.department_name, 
---  r.salary, 
---  m.manager_id,
---  m.first_name,
---  m.last_name
+SELECT 
+ e.id, 
+ e.first_name, 
+ e.last_name, 
+ r.title, 
+ d.department_name, 
+ r.salary, 
+ m.manager_id,
+ m.first_name,
+ m.last_name
  
--- FROM employee e
--- JOIN role r
---     ON e.role_id = r.id
--- JOIN department d 
---     ON d.id = r.department_id
--- LEFT JOIN employee m
---     ON e.manager_id = m.id; --
+FROM employee e
+JOIN role r
+    ON e.role_id = r.id
+JOIN department d 
+    ON d.id = r.department_id
+LEFT JOIN employee m
+    ON e.manager_id = m.id; 
 
 
--- --  allEmployeesByDept
+--  allEmployeesByDept
+SELECT d.department_name AS Department, e.first_name, e.last_name,
+FROM employee e
+JOIN role r ON e.role_id = r.id
+JOIN department d ON d.id = r.department_id
+
 
 
 -- WHEN I choose to VIEW ALL ROLES
 -- THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-Role - job title, salary, department id 
-Department - id, department_name
-Employee - id, first_name, last_name, role_id, manager_id
 
-SELECT r.title, e.role_id, d.department_name, r.salary
+
+SELECT r.title, e.role_id, d.department_name AS Department, r.salary
 FROM employee e JOIN role r ON e.role_id = r.id
 JOIN department d ON d.id = r.department_id 
+GROUP BY d.department_name
 
 -- WHEN I choose to ADD DEPARTMENT
 -- THEN I am prompted to enter the name of the department and that department is added to the database
+INSERT ON
+
+
 
 -- WHEN I choose to ADD ROLE
 -- THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
--- server.js
+++ server.js
@@ -106,7 +106,7 @@ function mainMenuQuestions() {
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 function allEmployees() {
   console.log("allEmployees")
-  let sql = 'SELECT e.id, e.first_name, e.last_name, r.title, d.department_name, r.salary, m.manager_id, m.first_name, m.last_name FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON e.manager_id = m.id;'
+  let sql = 'SELECT e.id, e.first_name, e.last_name, r.title, d.department_name, r.salary, m.manager_id, m.first_name, m.last_name FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON d.id = r.department_id LEFT JOIN employee m ON e.manager_id = m.id'
   db.query(sql, function(err, res) {
     if (err) {
       console.log(err);
@@ -148,7 +148,7 @@ function updateManager() {
 // WHEN I choose to view all roles
 // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 function viewAllRoles() {
-  let sql = 'select title from role';
+  let sql = 'SELECT r.title, e.role_id, d.department_name AS Department, r.salary FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON d.id = r.department_id GROUP BY d.department_name '
   db.query(sql, function(err, res) {
     if (err) {
       console.log(err);
@@ -183,8 +183,8 @@ function addDept() {
     }
 ])
   .then((answer) => {
-    let sql = `INSERT INTO department VALUES (${answer.department})`;
-    db.query(sql, { name: answer.department }, (err, res) => {
+    let sql = `INSERT INTO department (department_name) VALUES (${answer.department})`;
+    db.query(sql, (err, res) => {
       if (err) {
         console.log(err);
         return err;        
@@ -203,6 +203,15 @@ function allEmployeesByManager() {
 }
 
 function allEmployeesByDept() {
+  let sql = 'SELECT d.department_name AS Department, e.first_name, e.last_name FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON d.id = r.department_id';
+  db.query(sql, function(err, res) {
+    if (err) {
+      console.log(err);
+      return err;
+    }
+    console.table(res);
+    mainMenuQuestions();
+  });
 
 }
 
